fb3adc186ca4f19d5cc9ec3ca487207d556ba957
Kiana Mohseni <k.t.mohseni@gmail.com>
2020-07-15 18:57:14 -0700

    Update HashEntry.java
    commit

--- HashTable/src/TestApp/HashEntry.java
+++ HashTable/src/TestApp/HashEntry.java
@@ -12,3 +12,15 @@ public class HashEntry {
 		}
 	
 }
+public class HashEntry2 {
+		String key;
+		int value;
+		HashEntry next;
+		
+		HashEntry(String key, int value) {
+			this.key = key;
+			this.value = value;
+			this.next = null;
+		}
+	
+}
